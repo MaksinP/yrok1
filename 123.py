eac26ebe2abc6acefa7a67c7d5026de003122e42
Admin <polisuikmaksim44@gmail.com>
2023-01-13 17:15:33 +0200

    урок 4

--- 123.py
+++ 123.py
@@ -1,1 +1,15 @@
-print("дякую")
+result = []
+def divider(a, b):
+    if a < b:
+        raise ValueError
+    if b > 100:
+        raise IndexError
+    return a/b
+
+date = {10: 2, 2: 5, "123": 4, 18: 0, []: 15, 8 : 4}
+
+for key in data:
+    res = diveder(key, data[kem])
+    result.append(res)
+
+print(result )
